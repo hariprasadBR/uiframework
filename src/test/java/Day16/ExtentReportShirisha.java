9ae2bd7af32d7ed229fd80dc2b1cb8966044e07c
DELL <DELL@DESKTOP-Q69KFAD>
2025-06-24 23:50:15 +0530

    Shirisha:reportclass

--- src/test/java/Day16/ExtentReportShirisha.java
+++ src/test/java/Day16/ExtentReportShirisha.java
@@ -1,5 +1,17 @@
 package Day16;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReportShirisha {
+	
+public static void setup() {
+		
+		ExtentReports ex=new ExtentReports();
+//		ExtentSparkReporter exr = new ExtentSparkReporter(System.getProperty("user.dir")+"\\reports"+"\\File"+suitename+date()+".html");
+//		ex.attachReporter(exr);
+//		return ex;
+		
+	}
 
 }
